02870210784f964cdd590959691672dfcc132e1e
krzysztof.rzeszut <krzysztof.rzeszut@accenture.com>
2023-10-10 12:10:17 +0200

    5.3 done with comments

--- src/App.js
+++ src/App.js
@@ -1,16 +1,62 @@
-import './App.css';
-import Header from './components/Header/Header';
-import Menu from './components/Menu/Menu';
-import Hotels from './components/Hotels/Hotels';
+import React, { Component } from "react"; // musielismy zaimportowac Component
+import "./App.css";
+import Header from "./components/Header/Header";
+import Menu from "./components/Menu/Menu";
+import Hotels from "./components/Hotels/Hotels";
 
-function App() {
-  return (
-    <div className="App">
-      <Header />
-      <Menu />
-      <Hotels />
-    </div>
-  );
+// przerobimy ten komponent funkcyjny App na klasowy
+// function App() {
+//   return (
+//     <div className="App">
+//       <Header />
+//       <Menu />
+//       <Hotels />
+//     </div>
+//   );
+// }
+
+class App extends Component {
+  //    tworzymy state ale bez konstrukotra, bo poza stanem w tym konstruktorze nic nie robie
+  //    normalnie byłyb taki zapis w kompnenice klasowym:
+
+  //      constructor(props) {
+  //        super(props);
+  //        this.state = {
+
+  //    a to jest ponizje zapis skrocony
+  state = {
+    hotels: [
+      {
+        id: 1,
+        name: "Pod Akacjami",
+        city: "Warszawa",
+        rating: "8.3",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consequat id lorem vitae accumsan.",
+        image: "",
+      },
+      {
+        id: 2,
+        name: "Dębowy",
+        city: "Kraków",
+        rating: "8.8",
+        description:
+          "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consequat id lorem vitae accumsan.",
+        image: "",
+      },
+    ],
+  };
+  render() {
+    return (
+      <div className="App">
+        <Header />
+        <Menu />
+        {/* // musimy teraz ten stan przekazac do komponentu hotels, a wiec tworzymy sobie jakas wartosc np hotels i wrzucamy tam stat
+        // niemal wszedzie gdzie mamy komponety klasowe uzycie propsow, musimy uzyc slowa kluczowego this */}
+        <Hotels hotels={this.state.hotels} />
+      </div>
+    );
+  }
 }
 
 export default App;
--- src/components/Hotels/Hotel/Hotel.js
+++ src/components/Hotels/Hotel/Hotel.js
@@ -1,39 +1,46 @@
-import React from 'react';
-import styles from './Hotel.module.css';
-import hotelImg from '../../../assets/images/hotel.jpg'; 
+import React from "react";
+import styles from "./Hotel.module.css";
+import hotelImg from "../../../assets/images/hotel.jpg";
+import Hotels from "../Hotels";
 
-function Hotel() {
+//  tu mamy kompnent funkcyjny hotel i musimy my przekazac propsy ktore dostalismy z hotels
+function Hotel(props) {
   return (
     <div className={`card ${styles.hotel}`}>
       <div className="card-body">
-      
         <div className="row">
           <div className="col-4">
-            <img
-              src={hotelImg}
-              alt=""
-              className="img-fluid img-thumbnail" />
+            <img src={hotelImg} alt="" className="img-fluid img-thumbnail" />
           </div>
           <div className="col-8">
             <div className="row">
               <div className="col">
-                <p className={styles.title}>Pensjonat</p>
-                <span className="badge badge-light">Warszawa</span>
+                {/* // czyli tu juz nie bedzie na sztywno pensjonat // tylko bedzie
+                // to co dostaniemy z propsow, i tak podmieniamy wszystkie //
+                wartosci */}
+                {/* <p className={styles.title}>Pensjonat</p> */}
+                <p className={styles.title}>{props.name}</p>
+                {/* <span className="badge badge-light">Warszawa</span> */}
+                <span className="badge badge-light">{props.city}</span>
               </div>
               <div className="col text-right">
-                <h5>Ocena: 8.3</h5>
-                <a href="#" className="btn btn-primary mt-2 px-4">Pokaż</a>
+                {/* <h5>Ocena: 8.3</h5> */}
+                <h5>Ocena: {props.rating}</h5>
+                <a href="#" className="btn btn-primary mt-2 px-4">
+                  Pokaż
+                </a>
               </div>
             </div>
           </div>
-          
+
           <div className="col-12">
-            <p className={styles.description}>
-              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque consequat id lorem vitae accumsan. 
-            </p>
+            {/* <p className={styles.description}>
+              Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque
+              consequat id lorem vitae accumsan.
+            </p> */}
+            <p className={styles.description}>{props.description}</p>
           </div>
         </div>
-
       </div>
     </div>
   );
