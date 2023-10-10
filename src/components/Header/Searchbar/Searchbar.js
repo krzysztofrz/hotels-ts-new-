1503cb677e15cdb71c26c1ae3f3362cc700b03d8
krzysztof.rzeszut <krzysztof.rzeszut@accenture.com>
2023-10-10 10:30:39 +0200

    5.2 all

---
+++ src/components/Header/Searchbar/Searchbar.js
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+
+function Searchbar() {
+  const [term, setTerm] = useState('');
+
+  const search = () => {
+    console.log('szukaj!', term);
+  }
+  const onKeyDownHandler = e => {
+    if (e.key === 'Enter') {
+      search();
+    }
+  }
+
+  return (
+    <div className="d-flex">
+      <input
+        value={term}
+        onKeyDown={onKeyDownHandler}
+        onChange={e => setTerm(e.target.value)}
+        className="form-control"
+        type="text" 
+        placeholder="Szukaj..." />
+      <button
+        onClick={search}
+        className="ml-1 btn btn-secondary">
+          Szukaj
+      </button>
+    </div>
+  );
+}
+
+export default Searchbar;
